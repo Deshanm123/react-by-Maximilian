bfe3b580e0f80f38a44b25af1019a06f55189d25
Deshan Maduranga <deshanm123@gmail.com>
2024-04-06 22:01:00 +0100

    first commit

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -3,7 +3,7 @@ import React from 'react';
 const person = (props) => {
     return (
         <div>
-            <p>Hello Im {props.Name} & I'm  {props.Age} years old</p>
+            <p onClick={props.ClickHandler}>Hello Im {props.Name} & I'm  {props.Age} years old</p>
             <p>{props.children}</p>
         </div>
     );
